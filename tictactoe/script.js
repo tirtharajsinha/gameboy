cb6ba582066adc993e38609535bf5a1babf8dbb9
Tirtharaj Sinha <sinhatirtharaj@gmail.com>
2022-01-18 02:29:32 +0530

    added app

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -15,8 +15,22 @@ const winningConditions = [
   [2, 4, 6],
 ];
 
+var audio1 = new Audio("../audio/audio1.wav");
+var audio2 = new Audio("../audio/audio2.wav");
+var audio3 = new Audio("../audio/audio3.wav");
+var audio4 = new Audio("../audio/audio4.wav");
+var audio5 = new Audio("../audio/audio5.wav");
+setTimeout(() => {
+  audio3.play();
+}, 200);
+
 document.querySelectorAll(".row div").forEach((element) => {
   element.addEventListener("click", (event) => {
+    if (current_player == "X") {
+      audio1.play();
+    } else {
+      audio2.play();
+    }
     data = event.target.getAttribute("cell-data");
     if (player[data - 1] == "" && winner_not_decided) {
       event.target.innerHTML = current_player;
@@ -36,6 +50,7 @@ document.querySelectorAll(".row div").forEach((element) => {
       totalfree -= 1;
       if (totalfree == 0 && winner_not_decided) {
         winner_not_decided = false;
+        audio5.play();
         document.querySelector("body h3").innerHTML =
           "It's a draw.<br>Thank You for spending time with us";
       }
@@ -58,6 +73,7 @@ function checkwinner() {
     if (a === b && b === c) {
       roundWon = true;
       winner_not_decided = false;
+      audio4.play();
       break;
     }
   }
@@ -87,6 +103,7 @@ document.querySelector(".refresh").addEventListener("click", (event) => {
   totalfree = 9;
   winner_not_decided = true;
   document.querySelector("body h3").innerHTML = "It is X's turn.";
+  audio3.play();
 });
 
 if (document.body.clientWidth < 600) {
